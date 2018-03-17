3cc2b904c7ecc6bac8c5926d23b9ab15dc48f334
gongshen <gongshen1995@gmail.com>
2018-03-17 21:33:08 +0800

    echo

--- command/echo.go
+++ command/echo.go
@@ -8,10 +8,15 @@ import (
 
 func main() {
 	var s,sep string
+	for _,arg:=range os.Args[1:]{
+		s+=sep+arg
+	}
+	fmt.Println(s)
+	/*
 	for i:=0;i<len(os.Args);i++{
 		s+=sep+os.Args[i]
 		sep=" "
 	}
-	fmt.Println(s)
+	*/
 }
 
