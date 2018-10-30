c626e0d59483323bc642f1b2f03923f92c721e3f
gongshen <gongshen1995@gmail.com>
2018-10-30 22:15:08 +0800

    singalList

--- data_structure/单链表/link/link.go
+++ data_structure/单链表/link/link.go
@@ -1,47 +1,57 @@
-package link
+package list
 
 import (
 	"fmt"
 )
 
-type Node struct{
-	Data int
-	Next *Node
+type Element interface {}
+
+type node struct{
+	Data Element
+	Next *node
 }
 
-func (head *Node)Create() *Node{
+type SingalList interface {
+	//Front(head *node,new *node)	//在链头插入
+	Remove(head *node)SingalList	//删除
+	//Back()：在链尾插入
+	//Insert(head *node,index int,data Element)SingalList	//在指定位置插入
+	Insert(head *node) SingalList
+}
+
+func (head *node)Create() SingalList{
 	head = nil
 	return head
 }
 
-func (p *Node)PrintLink(){
+func (p *node)PrintLink(){
 	for p!=nil{
 		fmt.Printf("{Data:%d,Next:%p,Ptr:%p}\n",p.Data,p.Next,p)
 		p=p.Next
 	}
 }
 
-func (newNode *Node)Insert(head *Node)*Node{
-	var p0,p1 *Node
+func (newNode *node)Insert(head *node)SingalList{
+	var p0,p1 *node
 	p0=newNode
 	p1=head
-			if head==nil{
-				head=p0
-				head.Next=nil
+	if head==nil{
+		head=p0
+		head.Next=nil
 	}else{
 
 		for p1.Next != nil {
 			p1 = p1.Next
 		}
-			p1.Next = p0
-			p0.Next = nil
+		p1.Next = p0
+		p0.Next = nil
 
 	}
 	return head
 }
 
-func (delNode *Node)Delete(head *Node)*Node  {
-	var p1,p2 *Node
+func (delNode *node)Remove(head *node)SingalList  {
+	var p1,p2 *node
 	if head == nil{
 		fmt.Println("List is nil!")
 		return head
